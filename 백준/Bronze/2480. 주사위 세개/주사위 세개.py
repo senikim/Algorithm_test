615216ad094cd61128e331354a35a6e63c582805
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-03-02 20:48:10 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,9 @@
+a, b, c= list(map(int, input().split()))
+if a == b == c:
+    print(10000+a*1000)
+elif a == b or a == c:
+    print(1000+a*100)
+elif b == c:
+    print(1000+b*100)
+elif a != b != c:
+    print(max(a,b,c)*100)
