6cd52b0d7f4992c28db5c30ef420b1ffe2d3cbf0
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-06-21 16:10:57 +0900

    Update 달리기 경주.py

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -19,8 +19,34 @@ def solution(players, callings):
 
 
 # [Trial 1]
+def solution(players, callings):
+    answer = []
+    rank = []
 
+    for i in range(len(players)):
+      rank.append(i)
+    print(rank)
+    for g in range(len(callings)):
+      #print(rank[players.index(callings[g])])
+      rank[players.index(callings[g])] = rank[players.index(callings[g])]-1
+      rank[players.index(callings[g])-1] = rank[players.index(callings[g])-1]+1
+      print(rank)
+        #rank[callings[g]] = rank[callings[g]]-1
+#    print(rank)
+#        if callings[i] == index[]
+#    return answer
 
+'''
+[Trial]
+- players의 인덱스를 따와서 '등수' 딕셔너리->리스트 만듦 (처음엔 딕셔너리로 하려다가 리스트로 바꿔서 해봄)
+- callings 리스트 안에 있는 원소가 나올 때마다 '등수'배열에 있는 원소의 인덱스 -1 하고
+  반대로 원래 그 자리에 있던 원소의 인덱스는 +1 하려고 함.
+
+[Error]
+- 원래 등수에 있던 원소를 찾게 하는 코드를 유기적으로 설계하지 못함.
+- 'calling 리스트 안에 있는 원소 앞에 있던(리스트 기준 왼쪽에 위치한) 원소'를 '원래 등수에 있던 원소' 라고 설정했는데,
+  그러면 반복되는 calling 값은 연산이 중복됨 (당연함.....)
+'''
 
 # [Trial 2]
 def solution(players, callings):
