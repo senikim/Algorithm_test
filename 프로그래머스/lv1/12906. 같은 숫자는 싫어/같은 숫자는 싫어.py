270b03223b141ca79cbfe684f7ca25bdb60380b0
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-03 15:57:17 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -9,8 +9,8 @@ def solution(arr):
     return answer
 
 
-# TRIAL & ERROR
-# TRIAL1.
+
+# [TRIAL 1]
 def solution(arr):
     answer = []
     for i in range(1, len(arr)+1):
@@ -19,9 +19,38 @@ def solution(arr):
         else :
             pass
     answer.append(arr)
-    return answer
- 
+    return answer 
 '''
 # ERROR 1 : IndexError: list index out of range
 range 안맞음
+'''
+
+# [TRIAL 2]
+def solution(arr):
+    answer = []
+    for i in range(1, len(arr)):
+        if arr[i-1] == arr[i]:
+            answer.append(arr[i-1])
+        else :
+            answer.append(arr[i])
+    return answer  
+'''
+# Error 2. 중복되지 않는 값에 대한 조건 오류
+- if절까지만 하면 연속적으로 중복되는 값에 대해 제거가 가능한데
+- else에 포함되는 값은 추가가 안됨
+'''
+
+# [TRIAL 3]
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range(1, len(arr)):
+        answer.append(arr[i])
+        if answer[-1] == arr[i]:
+            answer.pop()
+    return answer
+'''
+# Error.
+- 항상 answer[-1] == arr[i]라서 결국 answer에 남아있는 값은 arr[0]값 뿐
+- answer[-2]로 해보면 다를까 싶음
 '''
