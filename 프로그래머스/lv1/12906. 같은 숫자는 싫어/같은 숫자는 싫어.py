a09da7007ca9b5db73899512fa3496f84daa486b
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-03 15:55:25 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -7,3 +7,21 @@ def solution(arr):
         else:
             answer.append(arr[i])
     return answer
+
+
+# TRIAL & ERROR
+# TRIAL1.
+def solution(arr):
+    answer = []
+    for i in range(1, len(arr)+1):
+        if arr[i-1] == arr[i]:
+            arr.pop(i)
+        else :
+            pass
+    answer.append(arr)
+    return answer
+ 
+'''
+# ERROR 1 : IndexError: list index out of range
+range 안맞음
+'''
