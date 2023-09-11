530d3cd066b128bcc335e352e89b9078649258c2
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-12 00:52:19 +0900

    Update 폰켓몬.py

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,23 @@
 # 다시 풀어보기
 
+# VER1.
 def solution(nums):
     unique = len(set(nums))
     if len(nums)/2 > unique:
         return unique
     else:
         return len(nums)/2
+
+# VER2.
+def solution(nums):
+    length = len(nums)/2
+    unique = len(set(nums))
+    return min(length, unique)
+
+# VER3.
+def solution(nums):
+    min(len(nums)/2, len(set(nums))
+
+'''
+set() : 중복 제거 함수
+'''
