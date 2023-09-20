1583fe491d3834028c4268d727efad63501481cf
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-20 23:29:13 +0900

    Update 배열 뒤집기.py

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -4,3 +4,16 @@ def solution(num_list):
         answer.append(num_list[-i])
     answer.append(num_list[0])
     return answer
+
+
+# 다른사람 풀이
+def solution(num_list):
+    answer = []
+    return num_list[::-1]
+
+def solution(num_list):
+    answer = []
+    while(num_list):
+        answer.append(num_list.pop())
+        # [num_list pop하면 뒤에서 부터 빠지니까 (선입선출) 그대로 answer에 append]
+    return answer
