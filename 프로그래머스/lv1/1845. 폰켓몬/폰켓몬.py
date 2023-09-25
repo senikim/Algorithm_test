64fff524033e582b38d8f041331f2a901e01c709
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-25 14:22:59 +0900

    Update 폰켓몬.py

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -10,3 +10,18 @@ def solution(nums):
         answer = len(nums)/2
     # 중복되지 않는 원소의 개수 > len(N/2) 이면 len(N/2)출력
     return answer
+
+
+# VER2.
+def solution(nums):
+    length = len(nums)/2
+    unique = len(set(nums))
+    return min(length, unique)
+
+# VER3.
+def solution(nums):
+    min(len(nums)/2, len(set(nums))
+
+'''
+set() : 중복 제거 함수
+'''
