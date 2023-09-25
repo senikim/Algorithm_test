0bc019be7138f202576ee93e0daba2195268f9c4
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-25 14:21:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.40 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,23 +1,12 @@
-# 다시 풀어보기
+# 가장 많은 종류의 폰켓몬 선택하는 방법
+# 중복되지 않는 원소의 개수 
 
-# VER1.
 def solution(nums):
-    unique = len(set(nums))
-    if len(nums)/2 > unique:
-        return unique
-    else:
-        return len(nums)/2
-
-# VER2.
-def solution(nums):
-    length = len(nums)/2
-    unique = len(set(nums))
-    return min(length, unique)
-
-# VER3.
-def solution(nums):
-    min(len(nums)/2, len(set(nums))
-
-'''
-set() : 중복 제거 함수
-'''
+    answer = 0
+    # 중복되지 않는 원소의 개수 < len(N/2)일 때 len(set(nums))
+    if len(set(nums)) <= len(nums)/2:
+        answer = len(set(nums))
+    elif len(set(nums)) > len(nums)/2:
+        answer = len(nums)/2
+    # 중복되지 않는 원소의 개수 > len(N/2) 이면 len(N/2)출력
+    return answer
