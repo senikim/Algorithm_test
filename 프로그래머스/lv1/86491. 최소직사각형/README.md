dc96d9b54a89f0cac5dc4ab6df32ab8bab65b913
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-09-28 18:05:30 +0900

    [level 1] Title: 최소직사각형, Time: 6.98 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 2.39 ms
+메모리: 11.3 MB, 시간: 6.98 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,14 +1,2 @@
 def solution(sizes):
-    answer = 0
-    w = []
-    h = []
-    for s in sizes:
-        if s[0] > s[1]:
-            w.append(s[0])
-            h.append(s[1])
-        else:
-            w.append(s[1])
-            h.append(s[0])
-    answer = max(w)*max(h)
-            
-    return answer
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
