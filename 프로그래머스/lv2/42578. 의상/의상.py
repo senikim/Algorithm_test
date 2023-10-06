d5de38425cd5a5d4c7cf795387160bb5a5c4ff5e
Seeun Kim <113660954+senikim@users.noreply.github.com>
2023-10-06 19:38:49 +0900

    Update 의상.py

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -14,3 +14,29 @@ def solution(clothes):
             # [마지막 +1 : 안입는 경우까지 포함]
         
     return answer - 1 #아무것도 입지 않는 경우 제외
+
+
+# Trial 1. 어떤 건 맞고 어떤 건 틀림 - 근데 일단 풀이 방법은 맞았음
+
+# 종류별로 나누고 그 개수에 +1(아예 안걸치는거), 종류별로 곱하고 -1(모든 종류가 0일 경우 제외)
+# 만약 종류가 하나밖에 없으면 개수 그대로 (최소 하나는 꼭 입어야 하니까)
+
+from collections import Counter
+def solution(clothes):
+    answer = 0
+    category = []
+    key = []
+    value = []
+
+    for c in clothes:
+        category.append(c[1])
+    count = Counter(category)
+    for k, v in count.items():
+        key.append(k)
+        value.append(v)
+
+    if len(key) > 1:
+        for v in range(len(value)):
+            value[v] += 1
+        return [value[i] * value[i+1] -1 for i in range(len(value)-1)][0]
+    return sum(value)
