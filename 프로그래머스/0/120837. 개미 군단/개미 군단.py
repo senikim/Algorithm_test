7e66fcc361bf08654e237654d0f056ea7a7f479c
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-03-28 22:45:24 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,22 @@
+# 장군 : 5, 병정 :3, 일개미 :1 의 공격력
+def solution(hp):
+    answer = 0
+    a, b, n1, n2, m1 = 0, 0, 0, 0, 0
+    
+    if hp%5 != 0:
+        a = hp%5
+        n1 = (hp-a)/5
+        if a%3 != 0 :
+            b = a%3
+            n2 = (a-b)/3
+            if b!=0:
+                n3 = b
+                return n1+n2+n3
+            return n1+n2
+        else:
+            m1 = a/3
+            return n1+m1
+    else :
+        return hp/5
+    
+    
