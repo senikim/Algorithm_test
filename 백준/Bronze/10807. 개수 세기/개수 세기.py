05a3deb83f1077bc43775af5a5f567ad6e25f816
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-07-11 17:13:52 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,3 +1,16 @@
+# Trial1. for문 이용하기
+# 시간복잡도 : O(N)
+N = input()
+numbers = list(map(int, input().split()))
+target = int(input())
+n = 0
+for i in numbers:
+    if i == target:
+        n+=1
+print(n)
+
+# Trial2. 리스트.count() 이용
+# 시간복잡도 : N
 N = input()
 numbers = list(map(int, input().split()))
 target = int(input())
