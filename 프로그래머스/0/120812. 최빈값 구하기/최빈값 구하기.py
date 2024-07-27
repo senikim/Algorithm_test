aafd51abdf81fc15be8aac17d41e53abf5b4221e
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-07-27 20:19:08 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,3 +1,17 @@
+# Trial1.
+def solution(array):
+    answer = 0
+    # 원소 set
+    answer_dict = {}
+    for i in set(array):
+        answer_dict[i] = array.count(i)
+    # value 값 중 가장 큰 key 출력
+    if len([k for k,v in answer_dict.items() if max(answer_dict.values())==v]) < 2:
+        return max(answer_dict, key=answer_dict.get)
+    else :
+        return -1
+
+# Trial2. 개선코드
 from collections import Counter
 def solution(array):
     answer = 0
