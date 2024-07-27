5ccc30f22b45f4e33683c8f8d87b9f3f811d6530
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-07-28 00:14:27 +0900

    Update 피자 나눠 먹기 （2）.py

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,7 +1,11 @@
 def solution(n):
-    slices_per_pizza = 6
-    lcm_value = slices_per_pizza
-    while lcm_value % n != 0:
-        lcm_value += slices_per_pizza
-    pizzas_needed = lcm_value // slices_per_pizza
-    return pizzas_needed
+    answer = 0
+    # n//최대공약수
+    # 최대공약수
+    num = 0
+    for i in range(min(6,n),1, -1):
+        if 6%i == 0 and n%i==0:
+            num = i
+            break
+    answer = n//num
+    return answer
