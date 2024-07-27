334c7520aa758e934f58e7e8f6a11de9826aa6f2
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-07-27 20:08:22 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,11 +1,9 @@
+from collections import Counter
 def solution(array):
     answer = 0
-    # 원소 set
-    answer_dict = {}
-    for i in set(array):
-        answer_dict[i] = array.count(i)
-    # value 값 중 가장 큰 key 출력
-    if len([k for k,v in answer_dict.items() if max(answer_dict.values())==v]) < 2:
-        return max(answer_dict, key=answer_dict.get)
-    else :
+    count = Counter(array)
+    mode = [k for k, v in count.items() if v == max(count.values())]
+    if len(mode) > 1:
         return -1
+    else:
+        return mode[0]
