ef3b61cff6698e22ea106c46f2175b6419984c29
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-09-11 16:01:48 +0900

    Update 짝수의 합.py

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,2 +1,13 @@
+# 다른 사람 풀이 참고
 def solution(n):
     return sum([i for i in range(2, n+1, 2)])
+
+
+# 원래 내 처음 풀이
+answer = 0
+for i in range(1,n+1):
+    if i%2==0:
+        answer+=i
+    else:
+        pass    # else 안해도 됨
+return answer
