aafec4d7c55400b897f3083f2f323b5edd43b00d
Seeun Kim <113660954+senikim@users.noreply.github.com>
2024-09-11 16:00:32 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = 0
-    for i in range(1,n+1):
-        if i%2==0:
-            answer+=i
-        else:
-            pass
-    return answer
+    return sum([i for i in range(2, n+1, 2)])
